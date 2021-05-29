5d1ccfb7ca35f85ae0e7f191993aaea348735bcb
up201706035 <up201706035@alunos.dcc.fc.up.pt>
2021-05-29 17:06:34 +0100

    a função para o entropy esta feita (só)

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import pandas
+import math
 from IPython.display import display
 
+input_file = "speedDating_trab.csv"
+df = pandas.read_csv(input_file, header=0)
+headers = list(df.columns.values)
+df = df._get_numeric_data()
 
-def id3():
+df.dropna(inplace=True)                          #elimina todas as linhas que tenham algum valor NA
+#print(df['length'][0:200])
+
+
+#vamos calcular todos os 0's w 1's do match
+one_match = 0                       #equivale ao 1
+zero_match = 0                       #equivalente a 0
+for index, rows in df.iterrows():
+    print(rows['match'])
+    if rows['match'] == 0.0:
+        zero_match = zero_match + 1
+    else:
+        one_match = one_match + 1
+
+
+def print_Table():
     # loading file
-    input_file = "speedDating_trab.csv"
-    df = pandas.read_csv(input_file, header=0)
-    headers = list(df.columns.values)
-    # df = df._get_numeric_data()
+    
     # numeric_headers = list(df.columns.values)
 
     with pandas.option_context('display.max_rows', 8377,
@@ -17,7 +34,18 @@ def id3():
                                'display.colheader_justify', 'left'):
         display(df.head(1000))
 
+#funcion for the entropy
+def entropy(one, zero):
+    inside_first = one/(one + zero)
+    inside_second = zero/(one + zero)
+    first_part = -inside_first * math.log2(inside_first)
+    second_part = inside_second * math.log2(inside_second)
+    return first_part - second_part
 
 # run script
 if __name__ == '__main__':
-    id3()
+    print_Table()
+
+print("1 nos matches: " + str(one_match) + " 0 nos matches: " + str(zero_match)) 
+
+print("motherfucker: " + str(entropy(one_match, zero_match)))                         #the global entropy
