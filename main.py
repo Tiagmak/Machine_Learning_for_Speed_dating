fb6fe86220fa639d7c60f8a769110891b5e97512
rainydaysavings <mail@eliseuama.ro>
2021-05-29 13:03:00 +0100

    Initial boilerplate.
    Signed-off-by: rainydaysavings <eliseuamaro@protonmail.com>

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas
+
+
+def load_file():
+    input_file = "speedDating_trab.csv"
+    df = pandas.read_csv(input_file, header=0)
+    original_headers = list(df.columns.values)
+    df = df._get_numeric_data()
+
+    # get headers
+    numeric_headers = list(df.columns.values)
+
+    print(f'Headers--, {numeric_headers}')
+    print(f'stuff--, {df}')
+
+
+# run script
+if __name__ == '__main__':
+    load_file()
