ccb7e4f9cb9e48e69344bbbe44f3d579472dbb26
rainydaysavings <eliseuamaro@protonmail.com>
2021-05-29 15:27:23 +0100

    Display now used.
    Signed-off-by: rainydaysavings <eliseuamaro@protonmail.com>

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import pandas
+from IPython.display import display
 
 
-def load_file():
+def id3():
+    # loading file
     input_file = "speedDating_trab.csv"
     df = pandas.read_csv(input_file, header=0)
-    original_headers = list(df.columns.values)
-    df = df._get_numeric_data()
+    headers = list(df.columns.values)
+    # df = df._get_numeric_data()
+    # numeric_headers = list(df.columns.values)
 
-    # get headers
-    numeric_headers = list(df.columns.values)
-
-    print(f'Headers--, {numeric_headers}')
-    print(f'stuff--, {df}')
+    display(df.head(8377))
 
 
 # run script
 if __name__ == '__main__':
-    load_file()
+    id3()
